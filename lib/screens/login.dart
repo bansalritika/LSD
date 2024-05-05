a55d7cc907753593eb087aba38e69c898b580c12
Payal Sharma <payalsharmareal@gmail.com>
2024-05-05 17:36:01 +0530

    created files for different screens

---
+++ lib/screens/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: Column(
+          children: [Text("Hello Login Screen")],
+          ),
+        ),        
+      ),
+    );
+  }
+}
