a98a41be9e3d1b0ecbc9414d6376feb54af3c7e1
AJAY KUMAR <kumarajay.rs3322@gmail.com>
2024-05-11 01:42:20 +0530

    updated by Aj

--- lib/screens/SendSMs.dart
+++ lib/screens/SendSMs.dart
@@ -63,7 +63,7 @@ class _SendSmsState extends State<SendSms> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> number = ['6203172968','9523137146'];
+    List<String> number = ['9523137146'];
     return Scaffold(
       appBar: AppBar(title: Text("Send Sms"),),
       body: ElevatedButton(
@@ -71,6 +71,7 @@ class _SendSmsState extends State<SendSms> {
           for(int i = 0 ; i<number.length;i++) {
             SmsStatus res = await BackgroundSms.sendMessage(
                 phoneNumber: number[i], message: 'Hell , I am Ajay \nEmergency! I need help urgently. My current location is https://www.google.com/maps?q=${_currentPosition?.latitude},${_currentPosition?.latitude}.\n Please send assistance immediately. Also, attaching a sound recording for better understanding of the situation. Please act fast. Thank you.');
+           print(res);
           }
         },
         child: Text("Send Sms"),
