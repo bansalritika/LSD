9aa2d8f60f6608d18a5dca80fb902d752a069b30
AJAY KUMAR <kumarajay.rs3322@gmail.com>
2024-05-11 01:49:37 +0530

    updated by Aj

--- lib/screens/SendSMs.dart
+++ lib/screens/SendSMs.dart
@@ -70,7 +70,8 @@ class _SendSmsState extends State<SendSms> {
         onPressed: () async {
           for(int i = 0 ; i<number.length;i++) {
             SmsStatus res = await BackgroundSms.sendMessage(
-                phoneNumber: number[i], message: 'Hii Ajay , \nEmergency! I need help urgently. My current location is https://www.google.com/maps?q=${_currentPosition?.latitude},${_currentPosition?.latitude} \n. Please send assistance immediately. Also, attaching a sound recording for better understanding of the situation. Please act fast. Thank you.');
+                phoneNumber: number[i], message: 'Hii Ajay \nEmergency! I need help urgently. My current location is https://www.google.com/maps?q=${_currentPosition?.latitude},${_currentPosition?.longitude}');
+           print(res);
           }
         },
         child: Text("Send Sms"),
